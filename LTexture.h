e59ad48f3d805e5e7373d20eb760a3d96e94cfc4
Nguyễn Đức Anh Quân <80147905+quan242@users.noreply.github.com>
2021-05-17 00:01:48 +0700

    Update LTexture.h

--- LTexture.h
+++ LTexture.h
@@ -1,28 +1,49 @@
-case SDLK_UP:
-        {
-            player.speed_y = 20;
-            while(player.speed_y > 0 && quit == false)
-            {
-                quit = check_collision(player, k);
-                player.y -= player.speed_y;
-                player.speed_y -= 2;
-                gBackgroundTexture.render(0,0,NULL);
-                rng(k,enemy);
-                player.animation( 7);
-                SDL_Delay(50);
-                cout<<player.x<<" "<<player.y<<endl;
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
 
-            }
-            while(player.y < 470 && quit == false)
-            {
-                quit = check_collision(player, k);
-                player.speed_y += 2;
-                player.y += player.speed_y;
-                gBackgroundTexture.render(0,0,NULL);
-                rng(k,enemy);
-                player.animation( 7);
-                SDL_Delay(50);
-                cout<<player.x<<" "<<player.y<<endl;
-            }
-            break;
-        }
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+
+class LTexture
+{
+    public:
+        //Initializes variables
+        LTexture();
+        //Deallocates memory
+        ~LTexture();
+        //Loads image at specified path
+        bool loadFromFile(string path, SDL_Renderer *gRenderer);
+        //Deallocates texture
+        void free();
+
+        void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+		//Set blending
+		void setBlendMode( SDL_BlendMode blending );
+
+		//Set alpha modulation
+		void setAlpha( Uint8 alpha );
+        //#if defined(SDL_TTF_MAJOR_VERSION)
+		//Creates image from font string
+		bool loadFromRenderedText(string textureText,TTF_Font *gFont, SDL_Color textColor, SDL_Renderer *gRenderer);
+		//#endif
+
+        void render( SDL_Renderer* gRenderer,int x, int y, SDL_Rect* clip = NULL);
+        //Gets image dimensions
+        int getWidth();
+        int getHeight();
+    private:
+        //The actual hardware texture
+        SDL_Texture* mTexture;
+        //Image dimensions
+        int mWidth;
+        int mHeight;
+};
+
+#endif // LTEXTURE_H
+
